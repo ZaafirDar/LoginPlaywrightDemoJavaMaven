1c00d9bc3df1e8b3aeff4c358d4da3dbf9adfcfa
Zaafir Dar <zaafirdar@Zaf-Macbook-Pro-M3.local>
2025-02-10 00:48:24 +0500

    Updating headless to true

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,7 +6,7 @@ public class App {
   public static void main(String[] args) {
     try (Playwright playwright = Playwright.create()) {
             
-            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
 
             BrowserContext context = browser.newContext();
             Page page = context.newPage();
