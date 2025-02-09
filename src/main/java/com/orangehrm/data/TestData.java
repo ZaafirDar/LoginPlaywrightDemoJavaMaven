5f2073814c43b08b8a94f97474910a044e70d50f
Zaafir Dar <zaafirdar@Zaf-Macbook-Pro-M3.local>
2025-02-10 02:08:21 +0500

    Adding Test Data file

---
+++ src/main/java/com/orangehrm/data/TestData.java
@@ -0,0 +1,15 @@
+package com.orangehrm.data;
+
+public class TestData {
+    // Base URL
+    public static final String BASE_URL = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
+
+    // Credentials
+    public static final String VALID_USERNAME = "Admin";
+    public static final String VALID_PASSWORD = "admin123";
+    public static final String INVALID_USERNAME = "InvalidUser";
+    public static final String INVALID_PASSWORD = "InvalidPass";
+
+    // Expected Messages
+    public static final String LOGIN_ERROR_MESSAGE = "Invalid credentials";
+}
--- src/test/java/com/orangehrm/tests/LoginTest.java
+++ src/test/java/com/orangehrm/tests/LoginTest.java
@@ -2,6 +2,7 @@ package com.orangehrm.tests;
 
 import com.microsoft.playwright.*;
 import com.orangehrm.pages.LoginPage;
+import com.orangehrm.data.TestData;
 import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,11 +24,21 @@ public class LoginTest {
     @Test
     public void testValidLogin() {
         loginPage.navigateToLoginPage();
-        loginPage.enterUsername("Admin");
-        loginPage.enterPassword("admin123");
+        loginPage.enterUsername(TestData.VALID_USERNAME); 
+        loginPage.enterPassword(TestData.VALID_PASSWORD);
         loginPage.clickLogin();
         
-        assertTrue(loginPage.isLoginSuccessful(), "Login failed!");
+        assertTrue(loginPage.isLoginSuccessful(), "Login Successful!");
+    }
+
+    @Test
+    public void testInvalidLogin() {
+        loginPage.navigateToLoginPage();
+        loginPage.enterUsername(TestData.INVALID_USERNAME);
+        loginPage.enterPassword(TestData.INVALID_PASSWORD);
+        loginPage.clickLogin();
+        
+        assertTrue(page.textContent(".oxd-alert-content").contains(TestData.LOGIN_ERROR_MESSAGE),"Invalid login credentials!");
     }
 
     @AfterEach
